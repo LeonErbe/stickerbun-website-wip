22d58f31aa8ca65955ea52c2889f88b1390548ee
Leon Erbe <leon.c.erbe@gmail.com>
2022-01-30 02:16:33 -0500

    Delete stickerbun.js
    Deleted as I've added it inside its respective folder.

--- stickerbun.js
+++
@@ -1,8 +0,0 @@
-$(function currentPage(){
-    $('a').each(function(){
-        if($(this).prop("href") == window.location.href) {
-            $(this).addClass("link-current");
-            $(this).parents("li").addClass("item-current");
-        }
-    });
-});
