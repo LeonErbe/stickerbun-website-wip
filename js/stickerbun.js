53e4a81d1172c6251602dc9d45cb2e855e96e8e5
Leon Erbe <leon.c.erbe@gmail.com>
2022-03-31 21:30:42 -0400

    Update stickerbun.js
    Mainly added a description of the function and transformed jQuery syntax to vanilla JavaScript - with the exception of the jQuery class selector, which I find very handy.

--- js/stickerbun.js
+++ js/stickerbun.js
@@ -1,8 +1,9 @@
-$(function currentPage(){
-    $('a').each(function(){
-        if($(this).prop("href") == window.location.href) {
-            $(this).addClass("link-current");
-            $(this).parents("li").addClass("item-current");
-        }
-    });
-});
+// highlights current tab in nav bar
+new function currentPage() {
+	$(".nav-link").each(function() {
+		if(this.href == window.location.href) {
+			this.classList.add("link-current");
+			this.parentNode.classList.add("item-current");
+		}
+	});
+}
